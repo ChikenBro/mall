dc5d93f7666353f98322b93c29a8cde185fe2caf
鸡哥 <602484813@qq.com>
2021-08-09 21:20:02 +0800

    ٩(•̤̀ᵕ•̤́๑)ᵎᵎᵎᵎ

---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="tab-bar">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBar",
+};
+</script>
+
+<style>
+#tab-bar {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+
+  display: flex;
+  background-color: #f6f6f6;
+
+  box-shadow: 0 -1px 1px rgba(0, 0, 0, 0.1);
+}
+</style>
